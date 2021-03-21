79514b48cb59016d9994448ae1f582dd9d0910f5
yrc <958480638@qq.com>
2021-03-21 17:25:21 +0800

    添加数据库操作

--- 221801312&221801337/Java/数据处理/src/main/java/com/data/papersearch/dao/PaperDao.java
+++ 221801312&221801337/Java/数据处理/src/main/java/com/data/papersearch/dao/PaperDao.java
@@ -1,4 +1,25 @@
 package com.data.papersearch.dao;
 
+import com.data.papersearch.pojo.Paper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface PaperDao {
+	public int getKeywordCount(String keyword);
+	public int getAuthorCount(String keyword);
+	public int getMeetCount(String keyword);
+	public int insertKeyword(String keyword);
+	public int insertMeet(String keyword);
+	public int insertAuthor(String keyword);
+	public int insertPaper(Object... o);
+	public int findMeetIdByName(String name);
+	public int findAuthorIdByName(String name);
+	public int findPaperIdByName(String name);
+	public int findKeywordIdByName(String name);
+
+	public int insertPaperAuthor(int paperId, int authorId);
+	public int insertPaperKeyword(int paperId, int keywordId);
+
 }
---
+++ 221801312&221801337/Java/数据处理/src/main/java/com/data/papersearch/service/Core.java
@@ -0,0 +1,115 @@
+package com.data.papersearch.service;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.data.papersearch.dao.PaperDao;
+import com.data.papersearch.pojo.Paper;
+import com.data.papersearch.pojo.PaperAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Core {
+	@Autowired
+	PaperDao dao;
+	PaperAdapter pa;
+	String path;
+	String meetName;
+	public Core(String path,String meetName,PaperAdapter pa){
+		this.pa=pa;
+		this.meetName=meetName;
+		this.path=path;
+	}
+
+	private List<Paper> getPapers() throws IOException {
+		File dir = new File(path);
+		File[] files = dir.listFiles();
+
+		ArrayList<Paper> papers = new ArrayList<>();
+		for (File file : files) {
+			StringBuilder sb = new StringBuilder();
+
+			FileReader fr = new FileReader(file);
+			Scanner scanner = new Scanner(fr);
+			//从文件读取
+			while(scanner.hasNextLine()){
+				sb.append(scanner.nextLine());
+			}
+			//去除最后的";"
+			if(sb.charAt(sb.length()-1)==';')
+				sb=sb.replace(sb.length()-1,sb.length(),"");
+
+			JSONObject jo = JSON.parseObject(sb.toString());
+			papers.add(pa.toPaper(jo,meetName,file));
+		}
+		return papers;
+	}
+	public void savePaper(Paper paper){
+		if(dao.getMeetCount(paper.getMeet())==0){
+			dao.insertMeet(paper.getMeet());
+		}
+		for (String keyword : paper.getKeywords()) {
+			if(dao.getKeywordCount(keyword)==0){
+				dao.insertKeyword(keyword);
+			}
+		}
+		for (String author : paper.getAuthors()) {
+			if(dao.getAuthorCount(author)==0){
+				dao.insertAuthor(author);
+			}
+		}
+		dao.insertPaper(
+				paper.getTitle(),
+				paper.getUrl(),
+				paper.getPublicationYear(),
+				paper.getAbstract(),
+				dao.findMeetIdByName(paper.getMeet()));
+		int paperId=dao.findPaperIdByName(paper.getTitle());
+		for (String author : paper.getAuthors()) {
+			dao.insertPaperAuthor(paperId,dao.findAuthorIdByName(author));
+		}
+		for (String keyword : paper.getKeywords()) {
+			dao.insertPaperKeyword(paperId,dao.findKeywordIdByName(keyword));
+		}
+
+	}
+	public void start(){
+		List<Paper> papers = null;
+		try {
+			papers = getPapers();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (Paper paper : papers) {
+			savePaper(paper);
+		}
+	}
+	public PaperAdapter getPa() {
+		return pa;
+	}
+
+	public void setPa(PaperAdapter pa) {
+		this.pa = pa;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getMeetName() {
+		return meetName;
+	}
+
+	public void setMeetName(String meetName) {
+		this.meetName = meetName;
+	}
+}
